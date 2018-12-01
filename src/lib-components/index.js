1c3873bb81def63ddbcec6b443638c5b807942b0
Bell Eapen <github@gulfdoctor.net>
2018-11-30 19:51:11 -0500

    first commit

---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as VueFhirSample } from './vue-fhir-sample.vue';
---
+++ src/lib-dev.vue
@@ -0,0 +1,23 @@
+<script>
+import { VueFhirSample } from './entry';
+export default {
+    name:  'VueFhirLibDev', // vue library dev component
+    components: {
+        VueFhirSample,
+    },
+}
+</script>
+
+<template>
+    <div class="vue-fhir-lib-dev">
+        <vue-fhir-sample></vue-fhir-sample>
+    </div>
+</template>
+
+<style scoped>
+    .vue-fhir-lib-dev {
+        display: block;
+        width: 400px;
+        margin: 25px auto;
+    }
+</style>
